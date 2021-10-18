9de2d16442eea2a272c4cb06352137e3e15c1ef9
KOSTA <KOSTA@DESKTOP-625RO36>
2021-10-18 11:13:44 +0900

    초기 커밋.

---
+++ WorkOut/frmLogIn.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WorkOut
+{
+    partial class frmLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbBoxName = new System.Windows.Forms.TextBox();
+            this.tbBoxPW = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NAME : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PW : ";
+            // 
+            // tbBoxName
+            // 
+            this.tbBoxName.Location = new System.Drawing.Point(148, 62);
+            this.tbBoxName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbBoxName.Name = "tbBoxName";
+            this.tbBoxName.Size = new System.Drawing.Size(88, 21);
+            this.tbBoxName.TabIndex = 2;
+            // 
+            // tbBoxPW
+            // 
+            this.tbBoxPW.Location = new System.Drawing.Point(148, 112);
+            this.tbBoxPW.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbBoxPW.Name = "tbBoxPW";
+            this.tbBoxPW.Size = new System.Drawing.Size(88, 21);
+            this.tbBoxPW.TabIndex = 3;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(246, 173);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(91, 31);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(130, 173);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(91, 31);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // frmLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 214);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.tbBoxPW);
+            this.Controls.Add(this.tbBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmLogIn";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        public System.Windows.Forms.TextBox tbBoxName;
+        public System.Windows.Forms.TextBox tbBoxPW;
+    }
+}
---
+++ WorkOut/frmLogIn.cs
@@ -0,0 +1,79 @@
+﻿using myLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkOut
+{
+    public partial class frmLogIn : Form
+    {
+        public frmLogIn()
+        {   
+            InitializeComponent();
+        }
+        /// <summary>
+        /// myLibraryURL : 각자의 mdf 파일 주소
+        /// </summary>
+        const string myLibraryURL = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\KOSTA\Desktop\WorkOut1-master\myDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+        SqlDB sqldb = new SqlDB(myLibraryURL);
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string name = tbBoxName.Text;
+            string pw = tbBoxPW.Text;
+
+            string ret = sqldb.GetString($"select name from WorkOutMem where name = '{name}'");
+            string ret2 = sqldb.GetString($"select PW from WorkOutMem where PW = '{pw}'and Name = '{name}' ");
+
+            form1 f1 = new form1();
+
+            if (name==ret)
+            {
+                if(pw==ret2)
+                {
+                    ///
+                    // 로그인 구현한 부분(추가 필요)
+                    ///
+                    MessageBox.Show($"환영합니다 {name}님");
+                    this.Hide();        // 숨기기
+                    switch(f1.ShowDialog())
+                    {
+                        case DialogResult.OK:
+                            f1.Close();
+                            break;
+
+                        case DialogResult.Cancel:
+                            Dispose();
+                            break;
+                    }
+
+                   
+                    
+                }
+                else if(pw!=ret2)
+                {
+                    MessageBox.Show("잘못된 PW입니다.");
+                    
+                }
+            }
+            else if(name!=ret)
+            {
+                MessageBox.Show("이름을 확인해주시길 바랍니다.");
+                
+            }
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
